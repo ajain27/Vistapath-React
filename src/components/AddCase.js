65d7f82dfadaa7c6fe2f52cb08c2d6aa42354e1a
Ankit Jain <ankit.jain67@t-mobile.com>
2020-12-15 16:11:43 -0800

    css changes and relaoding the page on submit to fetch the latest results

--- src/components/AddCase.js
+++ src/components/AddCase.js
@@ -90,7 +90,7 @@ function AddCase() {
 
     const handleCloseModal = () => {
         setisModalOpen(false);
-        // reloadPage();
+        reloadPage();
     }
 
     const reloadPage = () => {
